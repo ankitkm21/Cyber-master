e1b76a7631ff71b3436a1651456b96a98bad3988
ankitkm21 <30433288+ankitkm21@users.noreply.github.com>
2020-08-24 02:41:34 +0530

    Update Episode1.js

--- js/Episode1.js
+++ js/Episode1.js
@@ -1,6 +1,7 @@
 class Episode1{
     constructor(){}
     display(){
+        body.style.background = "white";
                var intro = createElement("h2");
                 intro.html("Season1:Episode1:Gmail Hacked!");
                 intro.position(500, 400);
